7a516eae7636cc8a4f54773c6478e8361e033558
FERNANDO CUNHA <101639298+omega3344@users.noreply.github.com>
2023-02-07 18:05:09 +0000

    tarefa 1 faltam 3

--- Tarefa1.sql
+++ Tarefa1.sql
@@ -8,13 +8,13 @@ CREATE VIEW P3 AS
 SELECT nome, numMotores FROM aviao JOIN modelo USING(idModelo);
 
 CREATE VIEW P4 AS
-SELECT COUNT(*) FROM voo WHERE duracao=2 OR duracao=3;
+SELECT COUNT(*) AS Total FROM voo WHERE duracao=2 OR duracao=3;
 
 CREATE VIEW P5 AS
 SELECT IdModelo, versao FROM modelo WHERE versao LIKE 'A3%';
 
 CREATE VIEW P6 AS
-SELECT idVoo, duracao FROM voo ORDER BY duracao DESC;
+SELECT idVoo, duracao FROM voo ORDER BY duracao DESC, idVoo;
 
 CREATE VIEW partidas AS
 SELECT idVoo AS idVooPartida, idAeroportoChegada AS idAeroporto  FROM voo WHERE idAeroportoPartida=1;
@@ -28,24 +28,32 @@ SELECT pais, COUNT(pais) AS total FROM aeroporto GROUP BY pais ORDER BY COUNT(pa
 
 9
 CREATE VIEW cidadePartidas AS
-SELECT idVoo, cidade FROM voo JOIN aeroporto ON idAeroporto=idAeroportoPartida ORDER BY idVoo;
+SELECT idVoo, cidade FROM voo JOIN aeroporto ON idAeroporto=idAeroportoPartida;
 CREATE VIEW cidadeChegadas AS 
-SELECT cidade FROM voo JOIN aeroporto ON idAeroporto=idAeroportoChegada ORDER BY idVoo;
+SELECT idVoo, cidade FROM voo JOIN aeroporto ON idAeroporto=idAeroportoChegada;
 CREATE VIEW P9 AS
-SELECT idVoo, cidadePartidas.cidade, cidadeChegadas.cidade FROM cidadePartidas, cidadeChegadas;
+SELECT cidadePartidas.idVoo, cidadePartidas.cidade AS 'cidade Partida', cidadeChegadas.cidade AS 'cidade Chegada' FROM cidadePartidas, cidadeChegadas WHERE cidadePartidas.idVoo = cidadeChegadas.idVoo ORDER BY cidadePartidas.idVoo;
 
 
 CREATE VIEW P10 AS
 SELECT idVoo FROM voo JOIN aeroporto ON idAeroporto=idAeroportoPartida AND idAeroportoPartida=1 AND idAeroportoChegada=3;
 
 CREATE VIEW P11 AS
-SELECT pais, COUNT(*) FROM aeroporto GROUP BY pais HAVING COUNT(pais)>2;
+SELECT pais, COUNT(*) AS total FROM aeroporto GROUP BY pais HAVING COUNT(pais)>2;
 
 12
+CREATE VIEW P12 AS
+SELECT pais, MIN(soma) AS total FROM (SELECT pais, COUNT(pais) AS soma FROM aeroporto GROUP BY pais) AS filtro;
 
 13
+CREATE VIEW P13 AS
+SELECT pais, MAX(soma) AS total FROM (SELECT pais, COUNT(pais) AS soma FROM aeroporto GROUP BY pais) AS filtro;
 
 CREATE VIEW P14 AS
-SELECT fabricante, versao, COUNT(versao) AS total FROM aviao JOIN modelo USING (idModelo) GROUP BY versao;
+SELECT fabricante, versao, COUNT(versao) AS total FROM aviao JOIN modelo USING (idModelo) GROUP BY versao ORDER BY COUNT(versao) DESC;
 
 15
+-- SELECT fabricante, versao, COUNT(aviao.idModelo) FROM aviao JOIN modelo USING(idModelo) GROUP BY idModelo;
+SELECT fabricante, versao, COUNT(soma) FROM modelo, (SELECT COUNT(idModelo) AS soma FROM aviao) AS tabela GROUP BY versao
+-- SELECT COUNT(idModelo) FROM aviao GROUP BY idModelo;;;;;;;
+SELECT fabricante, versao, COUNT(versao) AS total FROM modelo LEFT JOIN aviao USING (idModelo) GROUP BY versao ORDER BY total DESC;
